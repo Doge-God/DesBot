635e80c3c636bf67d91ce37150abc416615bef62
fz <futianzhou@hotmail.com>
2025-07-31 14:02:56 +1200

    init package & test nodes

---
+++ des_bot/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'des_bot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='fz',
+    maintainer_email='futianzhou@hotmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'vision_pub = des_bot.vision_pub:main',
+            'test_sub = des_bot.test_sub:main'
+        ],
+    },
+)
